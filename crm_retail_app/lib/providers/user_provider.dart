d74f6721bd7c18f45b11bacf0b0ba69070908674
Festim Beqiri <festimi-rrezoni@hotmail.com>
2025-07-16 10:31:47 +0200

    Add trusted device support with token

--- crm_retail_app/lib/providers/user_provider.dart
+++ crm_retail_app/lib/providers/user_provider.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider with ChangeNotifier {
-  static const _key = 'username';
+  static const _keyUsername = 'username';
+  static const _keyDeviceToken = 'device_token';
 
   String _username = '';
+  String _deviceToken = '';
   String get username => _username;
+  String get deviceToken => _deviceToken;
 
   UserProvider() {
     _loadUsername();
@@ -13,21 +16,31 @@ class UserProvider with ChangeNotifier {
 
   Future<void> _loadUsername() async {
     final prefs = await SharedPreferences.getInstance();
-    _username = prefs.getString(_key) ?? '';
+    _username = prefs.getString(_keyUsername) ?? '';
+    _deviceToken = prefs.getString(_keyDeviceToken) ?? '';
     notifyListeners();
   }
 
   Future<void> setUsername(String username) async {
     final prefs = await SharedPreferences.getInstance();
     _username = username;
-    await prefs.setString(_key, username);
+    await prefs.setString(_keyUsername, username);
+    notifyListeners();
+  }
+
+  Future<void> setDeviceToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    _deviceToken = token;
+    await prefs.setString(_keyDeviceToken, token);
     notifyListeners();
   }
 
   Future<void> clear() async {
     final prefs = await SharedPreferences.getInstance();
     _username = '';
-    await prefs.remove(_key);
+    _deviceToken = '';
+    await prefs.remove(_keyUsername);
+    await prefs.remove(_keyDeviceToken);
     notifyListeners();
   }
 }
