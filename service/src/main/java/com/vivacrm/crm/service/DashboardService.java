9c19fd6525880166370e8fc0ae5bd94ea1c7dbf0
Festim Beqiri <festimi-rrezoni@hotmail.com>
2025-08-15 13:55:36 +0200

    Connect to SQL Server and invoke stored procedure

--- service/src/main/java/com/vivacrm/crm/service/DashboardService.java
+++ service/src/main/java/com/vivacrm/crm/service/DashboardService.java
@@ -1,5 +1,6 @@
 package com.vivacrm.crm.service;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ import java.util.Map;
 public class DashboardService {
     private final JdbcTemplate jdbcTemplate;
 
-    public DashboardService(JdbcTemplate jdbcTemplate) {
+    public DashboardService(@Qualifier("sqlServerJdbcTemplate") JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -21,25 +22,31 @@ public class DashboardService {
             Map<String, Object> result = call.execute();
 
             return List.of(
-                    Map.of("title", "Total Revenue", "value", String.valueOf(result.getOrDefault("TotalRevenue", "0"))),
-                    Map.of("title", "Transactions", "value", String.valueOf(result.getOrDefault("Transactions", "0"))),
-                    Map.of("title", "Avg Basket Size", "value", String.valueOf(result.getOrDefault("AvgBasketSize", "0"))),
-                    Map.of("title", "Top Product Code", "value", String.valueOf(result.getOrDefault("TopProductCode", ""))),
-                    Map.of("title", "Top Product Name", "value", String.valueOf(result.getOrDefault("TopProductName", ""))),
-                    Map.of("title", "Returns Today", "value", String.valueOf(result.getOrDefault("ReturnsToday", "0"))),
-                    Map.of("title", "Low Inventory Count", "value", String.valueOf(result.getOrDefault("LowInventoryCount", "0")))
+                Map.of("title", "Total Revenue",
+                       "value", String.valueOf(result.getOrDefault("TotalRevenue", "0"))),
+                Map.of("title", "Transactions",
+                       "value", String.valueOf(result.getOrDefault("Transactions", "0"))),
+                Map.of("title", "Avg Basket Size",
+                       "value", String.valueOf(result.getOrDefault("AvgBasketSize", "0"))),
+                Map.of("title", "Top Product Code",
+                       "value", String.valueOf(result.getOrDefault("TopProductCode", ""))),
+                Map.of("title", "Top Product Name",
+                       "value", String.valueOf(result.getOrDefault("TopProductName", ""))),
+                Map.of("title", "Returns Today",
+                       "value", String.valueOf(result.getOrDefault("ReturnsToday", "0"))),
+                Map.of("title", "Low Inventory Count",
+                       "value", String.valueOf(result.getOrDefault("LowInventoryCount", "0")))
             );
         } catch (Exception e) {
             return List.of(
-                    Map.of("title", "Total Revenue", "value", "0"),
-                    Map.of("title", "Transactions", "value", "0"),
-                    Map.of("title", "Avg Basket Size", "value", "0"),
-                    Map.of("title", "Top Product Code", "value", ""),
-                    Map.of("title", "Top Product Name", "value", ""),
-                    Map.of("title", "Returns Today", "value", "0"),
-                    Map.of("title", "Low Inventory Count", "value", "0")
+                Map.of("title", "Total Revenue", "value", "0"),
+                Map.of("title", "Transactions", "value", "0"),
+                Map.of("title", "Avg Basket Size", "value", "0"),
+                Map.of("title", "Top Product Code", "value", ""),
+                Map.of("title", "Top Product Name", "value", ""),
+                Map.of("title", "Returns Today", "value", "0"),
+                Map.of("title", "Low Inventory Count", "value", "0")
             );
         }
     }
 }
-
